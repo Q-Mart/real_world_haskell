9431c785ae6c3fbee2afbc6740b238fe78faff56
Q-Mart <qumarthjash@gmail.com>
2021-04-17 14:01:31 +0100

    Let's try learning haskell again

--- ch01/WC.hs
+++ ch01/WC.hs
@@ -1,2 +1,7 @@
 main = interact wordCount
-    where wordCount input = show(length input) ++ "\n"
+  -- counts the number of lines
+  -- where wordCount input = show (length (lines input)) ++ "\n"
+  -- counts the number of words
+  -- where wordCount input = show (length (words input)) ++ "\n"
+  -- counts the number of chars
+  where wordCount input = show (length (input)) ++ "\n"
