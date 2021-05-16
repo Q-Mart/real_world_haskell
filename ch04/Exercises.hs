3929bc2af80beb1b8e7c687e944cf88c40d99df6
Q-Mart <qumarthjash@gmail.com>
2021-05-16 12:53:15 +0100

    Exercise 2-5

--- ch04/Exercises.hs
+++ ch04/Exercises.hs
@@ -30,4 +30,32 @@ asInt_fold xs
       digitsToInts xs' = foldl addDigit 0 xs'
       addDigit acc x = acc * 10 + digitToInt x
 
--- On exercise 2
+asInt_fold_error :: String -> Int
+asInt_fold_error xs
+  | xs == "" = error "Cannot call asInt_fold with empty string"
+  | xs == "-" = error "Cannot call asInt_fold with string \"-\""
+  | any (== '.') xs = error "Does not work with floating points"
+  | otherwise = asInt_fold xs
+
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either xs
+  | xs == "" = Left "Empty string"
+  | xs == "-" = Left "Non number '-'"
+  | any (== '.') xs = Left $ "Floating point " ++ xs
+  | otherwise = Right $ asInt_fold xs
+
+myConcat :: [[a]] -> [a]
+myConcat xs = foldr (++) [] xs
+
+takeWhileExplicit _ [] = []
+takeWhileExplicit f (x:xs)
+  | f x == True = [x] ++ takeWhileExplicit f xs
+  | otherwise = []
+
+takeWhileFold f xs = foldr appendIfTrue [] xs where
+  appendIfTrue x acc =  case f x of
+                         True -> x:acc
+                         False -> []
+
+-- On exercise 5
